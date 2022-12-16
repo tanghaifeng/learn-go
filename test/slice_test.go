22e7508d2f39f8af5f0a96614b06590582f3180f
TIM <tanghaifeng93@gmail.com>
2022-12-16 15:12:27 +0800

    slice

--- test/slice_test.go
+++ test/slice_test.go
@@ -1,10 +1,26 @@
 package test
 
 import (
-	"learn/collection"
+	"fmt"
 	"testing"
 )
 
 func TestSlice(t *testing.T) {
-	collection.Slice()
+	//collection.Slice()
+
+	book := make([]string, 1)
+	book = append(book, "童话故事", "老人与海")
+	test(book)
+	for _, v := range book {
+		fmt.Println(v)
+	}
+
+}
+
+func test(b []string) {
+
+	for _, v := range b {
+		fmt.Println(v)
+	}
+	b[1] = "老子"
 }
