92bd96b9091c019978c79f7da4e6496b49e762d6
tim <thf2345@163.com>
2023-03-28 00:02:46 +0800

    type

--- systemtype/struts.go
+++ systemtype/struts.go
@@ -7,6 +7,12 @@ type User struct {
 	Age  int
 }
 
+func (u *User) Write(p []byte) (n int, err error) {
+	fmt.Println(u.Name)
+	fmt.Println(u.Age)
+	return 0, nil
+}
+
 func (u *User) GetUserInfo() {
 	fmt.Println(u.Name)
 	fmt.Println(u.Age)
@@ -23,4 +29,10 @@ func (u *User) SetInfo(user User) {
 
 func GetUser(info UserInfo) {
 	info.GetUserInfo()
+}
+
+func (u *User) Read(p []byte) (n int, err error) {
+	fmt.Println(u.Name)
+	fmt.Println(u.Age)
+	return 0, nil
 }
--- test/systemtype_test.go
+++ test/systemtype_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"fmt"
 	"learn-go/systemtype"
 	"testing"
 )
@@ -9,15 +10,17 @@ func TestSystemType(t *testing.T) {
 
 	u := &systemtype.User{Name: "Tim", Age: 30}
 
-	systemtype.GetUser(u)
+	//systemtype.GetUser(u)
+	//
+	//u.SetInfo(systemtype.User{
+	//	Age: 22,
+	//})
+	//
+	//systemtype.GetUser(u)
+	//
+	//u1 := &systemtype.User{Name: "Joe", Age: 28}
+	//systemtype.GetUser(u1)
 
-	u.SetInfo(systemtype.User{
-		Age: 22,
-	})
-
-	systemtype.GetUser(u)
-
-	u1 := &systemtype.User{Name: "Joe", Age: 28}
-	systemtype.GetUser(u1)
-
+	u.Write([]byte("Hello"))
+	fmt.Fprintf(u, "world")
 }
